120ae90905084aaf9bae972761ad92e4de56868e
Hirohito Kato <hkato193@gmail.com>
2025-02-04 00:59:59 +0900

    Add sample p5.js code inside App.tsx

--- frontend/tiles/src/App.tsx
+++ frontend/tiles/src/App.tsx
@@ -1,10 +1,50 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ReactP5Wrapper, Sketch, SketchProps } from "@p5-wrapper/react";
+import './sketches/tile';
+
+type MySketchProps = SketchProps & {
+  rotation: number;
+};
+
+const sketch: Sketch<MySketchProps> = p5 => {
+  let rotation = 0;
+
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+  p5.updateWithProps = props => {
+    if (props.rotation) {
+      rotation = (props.rotation * Math.PI) / 180;
+    }
+  };
+
+  p5.draw = () => {
+    p5.background(100);
+    p5.normalMaterial();
+    p5.noStroke();
+    p5.push();
+    p5.rotateY(rotation);
+    p5.box(100);
+    p5.pop();
+  };
+};
 
 function App() {
   const [count, setCount] = useState(0)
+  const [rotation, setRotation] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setRotation(rotation => rotation + 100),
+      100
+    );
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <>
@@ -28,6 +68,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <ReactP5Wrapper sketch={sketch} rotation={rotation} />
     </>
   )
 }
